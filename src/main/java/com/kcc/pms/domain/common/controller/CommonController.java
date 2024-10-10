80d6f9e15877effd4fecd86e8bc0a93d1f2d7483
kkyh99 <154262006+kkyh99@users.noreply.github.com>
2024-10-10 15:18:45 +0900

    feat: 공통코드사용로직구현 (#31) (#36)
    * feat: 공통코드사용로직구현 (#31)
    
    * feat: dto명변경 (#31)

--- src/main/java/com/kcc/pms/domain/common/controller/CommonController.java
+++ src/main/java/com/kcc/pms/domain/common/controller/CommonController.java
@@ -1,14 +1,32 @@
 package com.kcc.pms.domain.common.controller;
 
+import com.kcc.pms.domain.common.model.dto.CommonCodeSelectList;
+import com.kcc.pms.domain.common.service.CommonService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 public class CommonController {
+
+    private final CommonService commonService;
 
     @GetMapping("/login")
     public String login() {
         return "login";
+    }
+
+
+    @GetMapping("/getCommonCodeList")
+    @ResponseBody
+    public List<CommonCodeSelectList> getCommonCodeList(@RequestParam("commonCodeNo") String commonCodeNo) {
+        System.out.println(commonCodeNo);
+        return commonService.getCommonCodeSelectList(commonCodeNo);
     }
 
 }
