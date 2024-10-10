ff17ea443f688a7ba2c9abe85cc7a6f2762ae548
kimyeonho <dksk87721@gmail.com>
2024-10-10 15:34:48 +0900

    feat: dto명 변경 (#37)

--- src/main/java/com/kcc/pms/domain/common/service/CommonService.java
+++ src/main/java/com/kcc/pms/domain/common/service/CommonService.java
@@ -1,9 +1,9 @@
 package com.kcc.pms.domain.common.service;
 
-import com.kcc.pms.domain.common.model.dto.CommonCodeSelectList;
+import com.kcc.pms.domain.common.model.dto.CommonCodeSelectListResponseDto;
 
 import java.util.List;
 
 public interface CommonService {
-    List<CommonCodeSelectList> getCommonCodeSelectList(String commonCodeNo);
+    List<CommonCodeSelectListResponseDto> getCommonCodeSelectList(String commonCodeNo);
 }
